ec773d72254466b62bb49d113587beb95392d8e1
Luís Henrique de Almeida <lubizinho-2001@hotmail.com>
2021-05-23 11:13:01 -0300

    localPlayer indentification, remove onunload and more entity randomization

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var port = 4444
 app.use(bodyParser.json())
 
 
+
 app.get('/', function(req, res) {
     res.sendfile("index.html");
 });
@@ -40,6 +41,18 @@ app.post('/updatePlayer', (req, res)=> {
     res.end()
 })
 
+app.post('/removePlayer', (req, res)=> {
+    var reqPlayer = req.body.player;
+    serverData.players.find(e=>e.id == reqPlayer.id).online = false;
+
+    console.log("removed: "+reqPlayer.id);
+    res.end()
+})
+
 app.listen(port, () => {
-  console.log(`Server Started`)
+    console.log('\x1b[33m%s\x1b[0m', "Server Started.");
+    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+        console.log("Ip Link:", '\x1b[36m', 'http://'+ add + ":" + port +"/",'\x1b[0m');
+        console.log("Localhost Link:", '\x1b[36m', 'http://localhost:' + port +"/",'\x1b[0m');
+    })
 })
